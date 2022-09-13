abf09d080b004c697857918cf63ae4b29b15975b
Piwo96 <philipp.rollmann@online.de>
2022-09-13 08:11:51 +0200

    app und pages setup

--- pages/sell-nft.tsx
+++ pages/sell-nft.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Sell NFT
     </div>
   )
 }
