8f6415c33c7c06923abf22cf8ac4703178732c81
Piwo96 <philipp.rollmann@online.de>
2022-09-14 23:26:07 +0200

    finished

--- components/NftBox.tsx
+++ components/NftBox.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { useWeb3Contract, useMoralis } from "react-moralis";
 import nftAbi from "../constants/BasicNft.json";
 import nftMarketplaceAbi from "../constants/NftMarketplace.json";
 import Image from "next/image";
-import { Card } from "web3uikit";
+import { Card, useNotification } from "web3uikit";
 import { ethers } from "ethers";
+import UpdateListingModal from "./UpdateListingModal";
 
 interface NftBoxProps {
     price: number,
@@ -15,12 +15,28 @@ interface NftBoxProps {
     seller: string,
 }
 
-const NftBox: NextPage<NftBoxProps> = ({price, nftAddress, tokenId, marketplaceAddress, seller}: NftBoxProps) => {
+const truncateStr = (fullStr: string, strLen: number) => {
+    if(fullStr.length <= strLen) return fullStr;
+
+    const separator = "...";
+    const separatorLen = separator.length;
+    const charsToShow = strLen - separatorLen;
+    const frontChars = Math.ceil(charsToShow/2);
+    const backChars = Math.floor(charsToShow/2);
+    return (
+        fullStr.substring(0, frontChars) + separator + fullStr.substring(fullStr.length - backChars)
+    )
+}
+
+const NftBox = ({price, nftAddress, tokenId, marketplaceAddress, seller}: NftBoxProps) => {
     const [imageURI, setImageURI] = useState("");
     const [tokenName, setTokenName] = useState("");
     const [toeknDescription, setTokenDescription] = useState("");
+    const [showModal, setShowModal] = useState(false);
+    const hideModal = () => setShowModal(false);
+    const dispatch = useNotification();
 
-    const { isWeb3Enabled } = useMoralis();
+    const { isWeb3Enabled, account } = useMoralis();
 
     const { runContractFunction: getTokenURI } = useWeb3Contract({
         abi: nftAbi,
@@ -30,6 +46,17 @@ const NftBox: NextPage<NftBoxProps> = ({price, nftAddress, tokenId, marketplaceA
             tokenId: tokenId,
         }
     }) 
+
+    const { runContractFunction: buyItem } = useWeb3Contract({
+        abi: nftMarketplaceAbi,
+        contractAddress: marketplaceAddress,
+        functionName: "buyItem",
+        msgValue: price,
+        params: {
+            nftAddress: nftAddress,
+            tokenId: tokenId
+        }
+    })
 
     async function updateUI(){
         const tokenURI = await getTokenURI();
@@ -51,16 +78,49 @@ const NftBox: NextPage<NftBoxProps> = ({price, nftAddress, tokenId, marketplaceA
         }
     }, [isWeb3Enabled])
 
+    const isOwnedByUser = seller === account || seller === undefined;
+    const formattedSeller = isOwnedByUser ? "you" : truncateStr(seller, 15);
+
+    const handleCardClick = () => {
+        isOwnedByUser
+            ? setShowModal(true)
+            : buyItem({
+                onError: (error) => {
+                    console.log(error);
+                },
+                onSuccess: () => {
+                    handleBuyItemSuccess();
+                }
+            });
+    }
+
+    const handleBuyItemSuccess = () => {
+        dispatch({
+            type: "success",
+            message: "Item bought!",
+            title: "Item Bought",
+            position: "topR"
+        })
+    }
+
     const renderImageURI = () => {
         return(
-            <div> 
+            <div className="mr-2"> 
+                <UpdateListingModal 
+                    isVisible={showModal}
+                    tokenId={tokenId}
+                    marketplaceAddress={marketplaceAddress}
+                    nftAddress={nftAddress}
+                    onClose={hideModal}
+                    />
                 <Card 
                     title={tokenName}
-                    description={toeknDescription}>
+                    description={toeknDescription}
+                    onClick={handleCardClick}>
                     <div className="p-2">
                         <div className="flex flex-col items-end gap-2">
                             <div>#{tokenId}</div>
-                            <div className="italic text-sm">Owned by {seller}</div>
+                            <div className="italic text-sm">Owned by {formattedSeller}</div>
                             <Image 
                                 loader={() => imageURI}
                                 src={imageURI}
